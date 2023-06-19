9bc5a9cfebd861abe6c6427a5787ca08ffbb5bab
Tao Yang <swulling@gmail.com>
2023-06-19 13:06:08 +0800

    增加文心百中离群值过滤机制

--- src/langchain_wenxin/retrievers.py
+++ src/langchain_wenxin/retrievers.py
@@ -5,6 +5,7 @@ import json
 from dataclasses import asdict, dataclass
 from typing import List, Optional, Tuple
 
+import numpy as np
 import requests
 from langchain.docstore.document import Document
 from langchain.schema import BaseRetriever
@@ -49,20 +50,26 @@ class Baizhong(BaseRetriever):
     """
     endpoint: str
     search_params: BaizhongSearchParams
+    # 使用离散值过滤的数量，如果设定，则先从百中中搜索出 filter_size 条结果，
+    # 再从这些结果中进行离散分布过滤，最终返回不超过 size 条数据
+    filter_size: int
     timeout: int
     para_decode_func = None
 
     def __init__(self, endpoint: str, search_params: BaizhongSearchParams,
-                 para_decode_func = para_decode, timeout: int = 10):
+                 para_decode_func = para_decode, timeout: int = 10, filter_size: int = 0):
         self.endpoint = endpoint
         self.search_params = search_params
         self.para_decode_func = para_decode_func
         self.timeout = timeout
+        self.filter_size = filter_size
 
     def get_relevant_documents(self, query: str) -> List[Document]:
         search_url = f"{self.endpoint}/baizhong/search-api"
         params = {k: v for k, v in asdict(self.search_params).items() if v is not None}
         params["q"] = query
+        if self.filter_size > 0:
+            params["size"] = self.filter_size
         r = requests.get(search_url, params=params, timeout=self.timeout)
         r.raise_for_status()
         res = r.json()
@@ -85,8 +92,36 @@ class Baizhong(BaseRetriever):
             )
             docs.append(doc)
 
+        if self.filter_size > 0:
+            # 对结果进行离散分布过滤
+            scores = [doc.metadata["_score"] for doc in docs]
+            outliers = find_outliers(scores, self.search_params.size)
+            return docs[:len(outliers)]
         return docs
 
     async def aget_relevant_documents(self, query: str) -> List[Document]:
         raise NotImplementedError
+
+
+def find_outliers(data: List[float], max_size: int):
+    """计算离群值，data 必须是一个倒序排列的数组"""
+    # 计算相邻元素的差值
+    diff = [data[i] - data[i+1] for i in range(len(data) - 1)]
+    # 计算平均差值
+    avg_diff = np.mean(diff)
+    # 离群值列表
+    outliers = []
+    # 从数组的开始处检查每个元素
+    for i in range(len(data) - 1):
+        # 如果元素与其后续元素的差值大于平均差值
+        if (data[i] - data[i+1]) > avg_diff:
+            outliers.append(data[i])
+            # 如果找到3个离群值则退出循环
+            if len(outliers) == max_size:
+                break
+    # 如果没有找到离群值，返回数组中的最大值作为一个离群值
+    if not outliers:
+        outliers.append(data[0])
+
+    return outliers
 
